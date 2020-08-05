d13319f0e3092743d4ac7a23d8cce9c867ee4d1c
郑万雷 <zhengwl@codoon.com>
2020-08-05 22:08:00 +0800

    209

--- 209/209_test.go
+++ 209/209_test.go
@@ -5,9 +5,37 @@ import (
 	"testing"
 )
 
-func minSubArrayLen(s int, nums []int) int {
+/*
+给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的 连续 子数组，并返回其长度。
+如果不存在符合条件的子数组，返回 0。
+*/
 
-	return 0
+func minSubArrayLen(s int, nums []int) int {
+	start := 0
+	end := 0
+	sum := 0
+	minArea := 2147483647 // int max
+	n := len(nums) - 1
+
+	for end <= n {
+		sum += nums[end]
+		for sum >= s {
+			area := end - start + 1
+
+			if area < minArea {
+				minArea = area
+			}
+
+			sum -= nums[start]
+			start++
+		}
+		end++
+	}
+
+	if minArea == 2147483647 {
+		return 0
+	}
+	return minArea
 }
 
 func Test345(t *testing.T) {
