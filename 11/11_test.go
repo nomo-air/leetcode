14ed5aa85627392f2835c921711e1f5843e095ed
郑万雷 <zhengwl@codoon.com>
2020-08-05 16:53:11 +0800

    11

--- 11/11_test.go
+++ 11/11_test.go
@@ -6,10 +6,34 @@ import (
 )
 
 func maxArea(height []int) int {
-	return 0
+	l := 0
+	r := len(height) - 1
+	maxArea := 0
+
+	for l < r {
+		area := min(height[l], height[r]) * (r - l)
+		if area > maxArea {
+			maxArea = area
+		}
+
+		if height[l] > height[r] {
+			r--
+		} else {
+			l++
+		}
+	}
+
+	return maxArea
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
 }
 
 func Test11(t *testing.T) {
-	n := maxArea([]int{1, 2})
+	n := maxArea([]int{2, 3, 4, 5, 18, 17, 6}) // 17
 	fmt.Printf("%+v", n)
 }
